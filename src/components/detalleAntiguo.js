d0c245d4c7e073ac05148c99db425d0eb9f15230
shift <teixeira.matheus2626@gmail.com>
2022-04-22 10:08:49 -0300

    bug corregido

--- src/components/detalleAntiguo.js
+++ src/components/detalleAntiguo.js
@@ -3,25 +3,19 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import image from '../components/image/pokedex.png'
 import './css/detalle.css';
-import { useSelector } from "react-redux";
 
 
 
 const Detalle = ()=>{
-    const[detalle, setDetalle] = useState(undefined)
+    const[detalle, setDetalle] = useState()
     const params = useParams()
-    const pokemons = useSelector(estado=>estado.pokemonsCopia2)
-
     useEffect(async()=>{
-        
-        if(params.id.length<10)setDetalle(pokemons.find(x=>x.id === parseInt(params.id)))
-        else setDetalle(pokemons.find(x=>x.id === params.id))
-        
-        console.log(pokemons)
-        console.log(params.id)
-        console.log(detalle)
-
-    }, [pokemons])
+        if(typeof parseInt(params.id) === 'number'){
+            const detalles = await axios.get(`https://pokedex-007.herokuapp.com/pokemons/${params.id}`);
+            setDetalle(detalles.data);
+        }
+  
+    }, [])
     return(
         <div>
             <div className="pokedexContainer"><img src={image} className='pokedex'/></div>
@@ -40,8 +34,8 @@ const Detalle = ()=>{
                     <p className="defensa">defensa:{detalle.defensa}</p>
                     <p className="fuerza">fuerza:{detalle.fuerza}</p>
                     <p className="vida">vida:{detalle.vida}</p>
-                    {detalle.createdinDb?<p className="tipo">tipos:{detalle.tipos.map(x=>x.nombre).join(',')}</p>:<p className="tipo">tipos:{detalle.tipos.join(',')}</p>}
-                    {detalle.createdinDb?<img src={`/assets/${detalle.tipos[0].nombre}.jpg`} className="fondoImg" alt='fondo'/>:<img src={`/assets/${detalle.tipos[0]}.jpg`} className="fondoImg" alt='fondo'/>}
+                    {detalle.createdinDb?<p className="tipo">tipos:{detalle.tipos.map(x=>x.nombre).join(',')}</p>:<p className="tipo">tipos:{detalle.tipo.join(',')}</p>}
+                    {detalle.createdinDb?<img src={`/assets/${detalle.tipos[0].nombre}.jpg`} className="fondoImg" alt='fondo'/>:<img src={`/assets/${detalle.tipo[0]}.jpg`} className="fondoImg" alt='fondo'/>}
                     {/*  */}
                     <NavLink to="/home" className='cerrar'>X</NavLink>
                 </div>
